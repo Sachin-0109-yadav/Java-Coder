70816bed12185fdbb29aa97060b40cadfccb9a72
Sachin <145640442+Sachin-0109-yadav@users.noreply.github.com>
2024-02-07 21:02:31 +0530

    Add files via upload

---
+++ Sachinyadav1.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Sachinyadav1 {
+    public static void main(String[] args) {
+        int age=34;
+        byte age2=56;
+        short age3=87;
+        long ageDino=5666666666666L;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.6D;
+        boolean a = true;
+        System.out.println(age);
+        String str = "Sachin";
+        System.out.println(str);
+    }
+}
