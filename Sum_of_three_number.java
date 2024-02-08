f4e89e93d2b4a2ed6f068af82e1ebe864e5f8f3a
Sachin <145640442+Sachin-0109-yadav@users.noreply.github.com>
2024-02-08 20:06:20 +0530

    Add files via upload

---
+++ Sum_of_three_number.java
@@ -0,0 +1,10 @@
+public class Sum_of_three_number {
+    public static void main(String[] args) {
+        int num1=7;
+        int num2=9;
+        int num3=123;
+        int sum=num1+num2+num3;
+        System.out.println("The sum is");
+        System.out.println(sum);
+    }
+}
