4c92b918132dc37f8be4945b5fc6aedb40838760
Sachin <145640442+Sachin-0109-yadav@users.noreply.github.com>
2024-02-12 07:53:05 +0530

    Add files via upload

---
+++ Resulting_data_type.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Resulting_data_type {
+    public static void main(String[] args) {
+       /* byte x = 5;
+        int y = 6;
+        short z = 8;
+        int a =45 + 4;
+        float b = 6.54f + x;
+        System.out.println(b);*/
+
+        // Increment and Decrement Operators
+        int i=56;
+//        int b = i++;  // first b is assigned  i (56) then I is incremented
+        int j=67;
+        int c=++j; // first j is incremented then c is assigned j (68)
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i);
+        int y = 7;
+        System.out.println(++y *8);
+        char ch ='a';
+        System.out.println(++ch);
+    }
+}
