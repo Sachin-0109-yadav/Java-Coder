5945bf28114b7b07ece5863307e2a86f5c46a278
Sachin <145640442+Sachin-0109-yadav@users.noreply.github.com>
2024-02-23 20:21:10 +0530

    Add files via upload

---
+++ For_loops.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class For_loops {
+    public static void main(String[] args) {
+        for (int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+        // 2n = Even Numbers = 0,2,4,6,8
+        // 2n+1 = Odd Numbers = 1,3,5,7,9
+
+        // Increasing Order
+        int n=5;
+        for (int i=0;i<n;i++){
+            System.out.println(2*i+1);
+        }
+    }
+}
