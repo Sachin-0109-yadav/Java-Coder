6a83667d5044537ab33b5215fd2564c340e18e05
Sachin <145640442+Sachin-0109-yadav@users.noreply.github.com>
2024-02-23 19:45:09 +0530

    Add files via upload

---
+++ Do_while_loop.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Do_while_loop {
+    public static void main(String[] args) {
+        // while loops
+//        int a=0;
+//        while (a<5){
+//            System.out.println(a);
+//            a++;
+//        }
+
+        // do-while-loop
+        int b=0;
+        do {
+            System.out.println(b);
+            b++;
+        }while (b<5);
+
+        int c=1;
+        do {
+            System.out.println(c);
+            c++;
+        }while (c<=10);
+    }
+}
