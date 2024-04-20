ed93659fb57f0edee4be787098ca868498b80513
Sachin <145640442+Sachin-0109-yadav@users.noreply.github.com>
2024-04-20 16:31:21 +0530

    Add files via upload

---
+++ Abstract_Class.java
@@ -0,0 +1,33 @@
+package com.company;
+
+abstract class Parent2{
+    public Parent2(){
+        System.out.println("Mai base2 ka constructor hoon");
+    }
+    public void sayHello(){
+        System.out.println("Hello");
+    }
+    abstract public void greet();
+    abstract public void greet2();
+}
+
+class Child2 extends Parent2{
+    @Override
+    public void greet(){
+        System.out.println("Good Morning");
+    }
+    public void greet2(){
+        System.out.println("Good afternoon");
+    }
+}
+
+abstract class  Child3 extends Parent2{
+    public void th(){
+        System.out.println("I am good");
+    }
+}
+public class Abstract_Class {
+    public static void main(String[] args) {
+     Child2 c = new Child2();
+    }
+}
